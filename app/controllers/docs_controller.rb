6be89791ba4a3717340fb429853c9808728b8421
parisa <parisa.golanbary@gmail.com>
2018-09-09 17:01:46 +0430

    Association between User and Files

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -8,18 +8,17 @@ class DocsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
-    @doc = Doc.new
-  end
+    @doc = current_user.docs.build
+  endurren
 
   def edit
   end
 
   def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
     if @doc.save
       redirect_to @doc
     else
@@ -49,4 +48,6 @@ class DocsController < ApplicationController
   def doc_params
     params.require(:doc).permit(:title, :content)
   end
+end
+
 end
--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# this is a Doc Model
 class Doc < ApplicationRecord
+  belongs_to :users
 end
